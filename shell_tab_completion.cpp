767f06f555344f0c775a432616dd3f170c244493
Mitch Waibel <mdub555@gmail.com>
2018-02-04 16:19:16 -0700

    Implimented tab completion.

--- shell_tab_completion.cpp
+++ shell_tab_completion.cpp
@@ -12,17 +12,100 @@
 #include <iostream>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <unistd.h>
+#include <dirent.h>
 
 using namespace std;
 
 
 void Shell::get_env_completions(const char* text, vector<string>& matches) {
-  // TODO: implement
+  // add the environment variables
+  // get the text string without the '$'
+  string textString = ((string)text).substr(1);
+  // loop through the environment variables
+  for (char** env = environ; *env != 0; env++) {
+    string envString = *env;
+    // find the '=' sign in the variable
+    string::size_type eq_pos = envString.find("=");
+    if (eq_pos == string::npos) continue;
+    // get everything before the '=' sign
+    else envString = envString.substr(0, eq_pos);
+    // check that it matches what we have so far
+    if (!envString.compare(0, textString.size(), textString)) {
+      matches.push_back("$" + envString);
+    }
+  }
+
+  // add the local variables
+  map<string, string>::iterator it;
+  // loop through all the aliases
+  for (it = localvars.begin(); it != localvars.end(); it++) {
+    // check if it mathces the string so far
+    if (!it->first.compare(0, textString.size(), textString)) {
+      matches.push_back("$" + it->first);
+    }
+  }
 }
 
 
 void Shell::get_command_completions(const char* text, vector<string>& matches) {
-  // TODO: implement
+  string textString = text;
+  // add the builtin commands
+  map<string, builtin_t>::iterator it1;
+  // loop through all builtins
+  for (it1 = builtins.begin(); it1 != builtins.end(); it1++) {
+    // check if it matches the string so far
+    if (!it1->first.compare(0, textString.size(), textString)) {
+      matches.push_back(it1->first);
+    }
+  }
+
+  // add the aliases
+  map<string, string>::iterator it2;
+  // loop through all the aliases
+  for (it2 = aliases.begin(); it2 != aliases.end(); it2++) {
+    // check if it mathces the string so far
+    if (!it2->first.compare(0, textString.size(), textString)) {
+      matches.push_back(it2->first);
+    }
+  }
+
+  // add the external commands
+  DIR           *dirp;      // needed when looping through all the $PATH directories
+  struct dirent *directory;
+  char* temp = getenv("PATH");
+  if (temp == NULL) {
+    cerr << __FUNCTION__ << ": $PATH does not exist" << endl;
+    return;
+  }
+  string path = (string)temp;
+  string::size_type col_pos;
+  // loop through the path and get the individual directories (seperated by colons)
+  do {
+    col_pos = path.find(":");
+    string subpath = path.substr(0, col_pos);
+    path = path.substr(col_pos + 1);
+    // go into the directory loop through all the files
+    dirp = opendir(subpath.c_str());
+    if (dirp) {
+      while ((directory = readdir(dirp)) != NULL) {
+        // check for being executabel
+        if (access(directory->d_name, X_OK)) {
+          string temp = (string)directory->d_name;
+          // check if it matches the string so far
+          if (!temp.compare(0, textString.size(), textString)) {
+            // add if to the vector
+            matches.push_back(temp);
+          }
+        }
+      }
+      if (closedir(dirp) != 0) {
+        return;
+      }
+    } else {
+      return;
+    }
+  } while (col_pos != string::npos);
 }
 
 
